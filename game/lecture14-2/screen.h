ae69442c77a5c4491dca1c00d456472b81c83977
202327068 <132327756+202327068@users.noreply.github.com>
2023-06-19 02:10:37 +0900

    Add files via upload

---
+++ game/lecture14-2/screen.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+void TitleScreen(void);
+void GameScreen(void);
+void GameOffScreen(void);
+void Two_90_Street_Screen(void);
+void Two_180_Street_Screen(void);
+void Three_Street_Screen(void);
+void Wall_Screen(void);
+void Snack_Screen(void);
+void Grass_Screen(void);
